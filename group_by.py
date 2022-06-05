7b720851a221614c7a876ff6a0f29a1226316ffd
AbedJoulany <59060418+AbedJoulany@users.noreply.github.com>
2022-06-05 12:16:02 +0300

    changing var names

--- group_by.py
+++ group_by.py
@@ -7,18 +7,18 @@ from collections.abc import Iterable
 """
 
 
-def group_by(func : callable , iterable : Iterable) -> dict:
+def group_by(function_to_call : callable , iter : Iterable) -> dict:
     """
     The function will return a dictionary while:
     The keys are the values returned from the function passed as the first parameter.
     The value to a particular key is a list of all the organs for which the value appearing in the key is repeated.
-    :param func: function to run on the values
-    :param iterable: iterable to get the values from
+    :param function_to_call: function to run on the values
+    :param iter: iterable to get the values from
     :return: dictionary
     """
     my_dict = {}
-    for iter in iterable:
-        my_dict.setdefault (func (iter), []).append (iter)
+    for iter in iter:
+        my_dict.setdefault (function_to_call (iter), []).append (iter)
     return my_dict
 
 
